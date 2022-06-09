d7623e32de7153874b2d7167451cb65797506e0d
VictorAndreiDan <48931547+VictorAndreiDan@users.noreply.github.com>
2022-06-10 00:16:38 +0300

    created the project, first test of deployment

--- Tutorials Completed/Blockchain Spotify Clone/src/backend/contracts/MusicNFTMarketplace.sol
+++ Tutorials Completed/Blockchain Spotify Clone/src/backend/contracts/MusicNFTMarketplace.sol
@@ -5,6 +5,57 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MusicNFTMarketplace is ERC721, Ownable{
+contract MusicNFTMarketplace is ERC721("DAppFi", "DAPP"), Ownable {
     string public baseURI = "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
+    string public baseExtension = ".json";
+    address public artist;
+    uint256 public royaltyFee;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        uint256 price;
+    }
+    MarketItem[] public marketItems;
+
+    event MarketItemBought(
+        uint256 indexed tokenId,
+        address indexed seller,
+        address buyer,
+        uint256 price
+    );
+
+    constructor(
+        uint256 _royaltyFee,
+        address _artist,
+        uint256[] memory _prices
+    ) payable {
+    require(
+            _prices.length * _royaltyFee <= msg.value,
+            "Deployer must pay royalty fee for each token listed on the marketplace"
+        );
+        royaltyFee = _royaltyFee;
+        artist = _artist;
+        for (uint8 i = 0; i < _prices.length; i++) {
+            require(_prices[i] > 0, "Price must be greater than 0");
+            _mint(address(this), i);
+            marketItems.push(MarketItem(i, payable(msg.sender), _prices[i]));
+        }
+    }
+    //only DEPLOYER of contract can change its royalty fee
+    function updateRoyaltyFee(uint256 _royaltyFee) external onlyOwner{
+        royaltyFee = _royaltyFee;
+    }
+    //function for buying nfts
+    function buyToken(uint256 _tokenId) external payable{
+        uint256 price = marketItems[_tokenId].price;
+        address seller = marketItems[_tokenId].seller;
+        require(
+            msg.value == price, "Please send the asking price for the NFT"
+        );
+        marketItems[_tokenId].seller = payable(address(0));
+        _transfer(address(this), msg.sender, _tokenId);
+        payable(artist).transfer(royaltyFee);
+        payable(seller).transfer(price);
+    }
 }
