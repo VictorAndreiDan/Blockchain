9e5b792297904ed0ea85914688d92b131ed4e974
VictorAndreiDan <48931547+VictorAndreiDan@users.noreply.github.com>
2022-07-06 23:03:18 +0300

    created more functionality for marketplace contract - creating items, events

--- Tutorials Completed/NFT Marketplace/src/backend/test/NFTMarketplace.test.js
+++ Tutorials Completed/NFT Marketplace/src/backend/test/NFTMarketplace.test.js
@@ -4,6 +4,7 @@ const { expect } = require("chai");
 describe("NFTMarketplace", function () {
     //constants
     let deployer, addr1, addr2, nft, marketplace;
+    let URI = "Sample URI";
     beforeEach(async function (){
         //factories
         const NFT = await ethers.getContractFactory("NFT");
@@ -20,6 +21,27 @@ describe("NFTMarketplace", function () {
         it("Should track name and symbol of nft", async function(){
             expect(await nft.name()).to.equal("newNftName")
             expect(await nft.symbol()).to.equal("NNN")
+        })
+        //test fees
+        it("Should track name and symbol of nft", async function(){
+            expect(await marketplace.feePercent()).to.equal(1)
+            expect(await marketplace.feeAccount()).to.equal(deployer.address)
+        })
+    })
+    //test mint
+    describe("Mint", function (){
+        //minting
+        it("Should track each minted nft", async function(){
+            //addr1
+            await nft.connect(addr1).mint(URI);
+            expect(await nft.tokenCount()).to.equal(1);
+            expect(await nft.balanceOf(addr1.address)).to.equal(1);
+            expect(await nft.tokenURI(1)).to.equal(URI);
+            //addr2
+            await nft.connect(addr2).mint(URI);
+            expect(await nft.tokenCount()).to.equal(2);
+            expect(await nft.balanceOf(addr2.address)).to.equal(1);
+            expect(await nft.tokenURI(2)).to.equal(URI);
         })
     })
 });
